8a99ee7c68bfec40e1603f3bf627f294241669a1
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2024-07-22 20:11:45 +0700

    add files

---
+++ src/javascript.js
@@ -0,0 +1,10 @@
+window.onload = fadeIn;
+
+function fadeIn(){
+    var nav = document.getElementById('navbar');
+    var iframe = document.getElementById('iframes');
+    var headerNewVid = document.getElementById('header_newest_video');
+    nav.style.opacity = 1;
+    iframe.style.opacity = 1;
+    headerNewVid.style.opacity = 1;
+}
